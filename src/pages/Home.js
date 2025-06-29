ac408051579f31a7ab2d9b7ac54adce887b58e60
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:03:12 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Welcome to Velvet Circle</h1>
+      <p>Premium companionship with a velvet touch.</p>
+    </div>
+  );
+}
 
