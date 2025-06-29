58429b5085f13dc93d318456690ce91c14d84169
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:04:57 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-velvet text-center p-4 mt-8">
+      Velvet Circle &copy; {new Date().getFullYear()}
+    </footer>
+  );
+}
 
