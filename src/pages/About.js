acf9102516a12120cd6a62a4d6045ac3bf6062d7
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:03:25 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,1 +1,9 @@
+export default function About() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Velvet Circle</h1>
+      <p>We provide companionship services in a respectful, gender-neutral, inclusive manner.</p>
+    </div>
+  );
+}
 
