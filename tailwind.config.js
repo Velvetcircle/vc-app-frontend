93ea8257635bb0510415a66f628844d5f4a74b19
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:00:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        velvet: "#4b0082"
+      }
+    },
+  },
+  plugins: [],
+}
 
