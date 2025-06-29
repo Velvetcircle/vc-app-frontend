2849f2ac2462908c17990ceb2930cbc8d90924bd
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 13:04:55 +0530

    Update VolunteerApplication.js

--- src/pages/VolunteerApplication.js
+++ src/pages/VolunteerApplication.js
@@ -1,1 +1,25 @@
+export default function VolunteerApplication() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto text-white space-y-6">
+      <h1 className="text-3xl font-bold mb-4">🤝 Ready to Join Us?</h1>
+      <p>
+        We’re thrilled that you want to become a Velvet Circle volunteer companion. Please fill out
+        the application form below. Your information will remain confidential and only be used to
+        verify your eligibility.
+      </p>
+      <div className="mt-6">
+        <iframe
+          src="https://docs.google.com/forms/d/e/your-google-form-id/viewform?embedded=true"
+          width="100%"
+          height="1200"
+          frameBorder="0"
+          className="rounded-xl shadow-lg"
+          title="Velvet Circle Volunteer Application"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
+}
 
