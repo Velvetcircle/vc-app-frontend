7983ffdf2f61513d4ac817c8f600ab6a19c76c44
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 14:29:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import App from "./App";
+import "./index.css";   // Tailwind base
+import "./styles/velvet.css"; // (optional custom branding CSS if you made one)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
 
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
