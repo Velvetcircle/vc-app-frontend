d593e932568fa28bc92a3fd3a5371af84a9dc667
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:04:15 +0530

    Update Terms.js

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -1,1 +1,14 @@
+export default function Services() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Our Services</h1>
+      <ul className="list-disc ml-6">
+        <li>Voice companionship - ₹5/min</li>
+        <li>Chat companionship - ₹3/min</li>
+        <li>Video companionship - ₹10/min</li>
+        <li>Physical companionship - ₹150/hr</li>
+      </ul>
+    </div>
+  );
+}
 
