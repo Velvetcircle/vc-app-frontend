8632339c53758726e40ef08e62ca62f8d33dcff5
Velvetcircle <velvetcircleindia@gmail.com>
2025-06-29 12:04:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-velvet p-4 flex justify-between items-center">
+      <div className="text-xl font-bold">Velvet Circle</div>
+      <div className="space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/booking">Book</Link>
+        <Link to="/volunteer">Volunteer</Link>
+      </div>
+    </nav>
+  );
+}
 
