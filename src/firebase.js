b9e22bf3659a5a01ff8b1630226d14d2e425fd40
Velvetcircle <velvetcircleindia@gmail.com>
2025-07-02 11:15:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,10 +8,12 @@ const firebaseConfig = {
   authDomain: "the-velvet-circle.firebaseapp.com",
   projectId: "the-velvet-circle",
   storageBucket: "the-velvet-circle.appspot.com",
-  messagingSenderId: "your_sender_id",  // optional for now
-  appId: "your_app_id" // optional for now
+  messagingSenderId: "your_sender_id", // optional, you can remove if not needed
+  appId: "your_app_id" // optional, you can remove if not needed
 };
 
 const app = initializeApp(firebaseConfig);
+
+// export reusable Firebase features
 export const auth = getAuth(app);
 export const db = getFirestore(app);
